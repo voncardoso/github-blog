ed6bbb9a76de32ccbcdace394567547462082abe
voncardoso <vonharrison26@gmail.com>
2023-01-29 12:18:53 -0300

    styled-components setup finished

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
+import { ThemeProvider } from "styled-components";
+import { GlobalStyle } from "./Style/global";
+import { defaultTheme } from "./Style/theme/default";
+
 function App() {
   return (
-    <div className="App">
-      <h1>Hello word</h1>
-    </div>
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
+      <div className="App">
+        <h1>Hello word</h1>
+      </div>
+    </ThemeProvider>
   );
 }
 
