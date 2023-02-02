4b4ab169ba9c7cb0c860233fb187252cbc2bffc6
Von Harrison <vonharrison26@gmail.com>
2023-02-02 16:28:50 -0300

    seach cretae fecth

--- src/Page/Home/components/Seach/index.tsx
+++ src/Page/Home/components/Seach/index.tsx
@@ -1,13 +1,24 @@
+import { useState } from "react";
+import { useContextSelector } from "use-context-selector";
+import { UserContext } from "../../../../context/UserContext";
 import { SeachContainer, SeachHeader, SeachInput } from "./style";
 
 export function Seach() {
+  const [seach, setSeach] = useState("")
+  const fechIssues = useContextSelector(UserContext, (context) => {
+    return context.FechIssues;
+  });
+
+  
+  fechIssues(seach)
+
   return (
     <SeachContainer>
       <SeachHeader>
         <strong>Publicações</strong>
         <p>6 publicações</p>
       </SeachHeader>
-      <SeachInput placeholder="Buscar conteúdo" type="text" />
+      <SeachInput placeholder="Buscar conteúdo" type="text" onChange={(event) => setSeach(event.target.value)}/>
     </SeachContainer>
   );
 }
